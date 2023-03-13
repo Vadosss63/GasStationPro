b1df1f39b656ecd757a33e3321ef4ed9d2173783
Vadosss63 <parusovvadim@mail.ru>
2023-03-14 00:46:17 +0200

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
-install_dir="/opt/GasStationPro/"
+install_dir="/opt/GasStationPro"
+working_irectory=".GasStationPro"
 #Add user to dialout(COM port)
 sudo usermod -a -G dialout $USER
 
-if ! [ -d ${install_dir} ]; then
-sudo mkdir ${install_dir}
+if [ -d ${install_dir} ]; then
+sudo systemctl stop gas_station.service
+sudo systemctl disable gas_station
+sudo rm -rf ${install_dir}
+sudo rm -rf ~/${working_irectory}
 fi
+sudo mkdir ${install_dir} 
+mkdir ~/${working_irectory}
 
 #Build GasTeminal
-pushd .
 cd GasTeminal/gas_station
 if [ -d build ]; then
 rm -rf build
 fi
 
 cp -r settings/* ~/.GasStationPro
+
 mkdir build
 cd build/
 qmake ../
 make -j4
-sudo mv gas_station ${install_dir}
+
+sudo mv gas_station ${install_dir}/
 
 ### create gas_station.service 
 printf "[Unit]
@@ -28,20 +35,17 @@ Description=gas_station
 After=network.target
 
 [Service]
-ExecStart=/opt/GasStationPro/gas_station
-ExecReload=/opt/GasStationPro/gas_station
-WorkingDirectory=/home/%s/.GasStationPro/
+ExecStart=%s/gas_station
+ExecReload=%s/gas_station
+WorkingDirectory=/home/%s/%s
 Restart=always
 
 [Install]
-WantedBy=graphical-session.target" ${USER} >> gas_station.service
+WantedBy=graphical-session.target" ${install_dir} ${install_dir} ${USER} ${working_irectory} > gas_station.service
 ### end create gas_station.service 
 
-sudo cp gas_station.service ${install_dir}
-sudo ln -s -f ${install_dir}gas_station.service /etc/systemd/user/gas_station.service
-
-popd
-popd
+sudo mv gas_station.service ${install_dir}/
+sudo ln -s -f ${install_dir}/gas_station.service /etc/systemd/user/gas_station.service
 
 #Run GUI demon
 systemctl --user enable gas_station
--- installDep.sh
+++ installDep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install git g++ qmake qtcreator qt5-qmake qt5-qmake-bin qt5 build-essential
+sudo apt install sudo git g++ qmake qtcreator qt5-qmake qt5-qmake-bin qt5 build-essential
 sudo apt install qt5-default qt6-default qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
 sudo apt install equivs libqt5serialport5 libqt5serialport5-dev
-sudo apt install golang
+
